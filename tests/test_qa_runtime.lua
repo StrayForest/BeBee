9cd5a2efc03d626ab2860d2186b790b488473949
Aleksei Lisitcin <157023683+StrayForest@users.noreply.github.com>
2026-08-29 00:47:14 +0300

    BB-006 add deterministic QA request tests

---
+++ tests/test_qa_runtime.lua
@@ -0,0 +1,44 @@
+local qa = require "app.qa_runtime"
+local test = require "tests.testlib"
+
+local function supported_foundation_state()
+    test.assert_true(qa.is_supported_state("foundation_probe"))
+    test.assert_false(qa.is_supported_state("movement_empty"))
+end
+
+local function default_seed()
+    test.assert_equal(88008, qa.normalize_seed(nil))
+    test.assert_equal(88008, qa.normalize_seed(""))
+end
+
+local function normalized_seed()
+    test.assert_equal(42, qa.normalize_seed("42.9"))
+    test.assert_equal(7, qa.normalize_seed(-7))
+end
+
+local function supported_request()
+    local request = qa.resolve_request("foundation_probe", "123")
+    test.assert_equal("foundation_probe", request.state_id)
+    test.assert_equal(123, request.seed)
+    test.assert_true(request.supported)
+    test.assert_equal(nil, request.error)
+end
+
+local function unknown_state_fails_closed()
+    local request = qa.resolve_request("movement_empty", "5")
+    test.assert_equal("movement_empty", request.state_id)
+    test.assert_equal(5, request.seed)
+    test.assert_false(request.supported)
+    test.assert_equal("unknown_state", request.error)
+end
+
+return {
+    name = "qa_runtime",
+    cases = {
+        { name = "supported_foundation_state", run = supported_foundation_state },
+        { name = "default_seed", run = default_seed },
+        { name = "normalized_seed", run = normalized_seed },
+        { name = "supported_request", run = supported_request },
+        { name = "unknown_state_fails_closed", run = unknown_state_fails_closed },
+    },
+}
